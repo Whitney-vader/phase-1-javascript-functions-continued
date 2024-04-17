75039a178fcfcc81d1f6d0966f948c9385fe9471
Whitney_vader <whitney@gmail.com>
2024-04-17 11:36:11 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(wrapAdjective("%")("a dedicated programmer")); // "You are %a dedicated programmer%!"
+  console.log(encouragingPromptFunction("an excellent teacher")); // "You are !!!an excellent teacher!!!"
+  
+  
